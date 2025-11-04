59eba94e38769025eff2b5b6b5d1b2407c4cf1a1
al1enn <wipetmp1404@proton.me>
2025-11-04 19:05:51 +0500

    create bot isntance

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import settings
+from routers import router
+
+bot = Bot(token=str(settings.api_token))
+dp = Dispatcher()
+
+
+async def main():
+    print("I start polling")
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
