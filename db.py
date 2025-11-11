0d7acc9ced6f029f9593dc45a0363f74e9120943
al1enn <wipetmp1404@proton.me>
2025-11-11 18:46:22 +0500

    create users table if not exists

--- db.py
+++ db.py
@@ -4,15 +4,15 @@ import sqlite3
 connection = sqlite3.connect("mydatabase.db")
 
 cursor = connection.cursor()
-# session.execute(
-#     """
-# CREATE TABLE  users (
-# id INTEGER PRIMARY KEY AUTOINCREMENT,
-# username TEXT,
-# email TEXT NOT NULL UNIQUE
-# );
-# """
-# )
+cursor.execute(
+    """
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+email TEXT NOT NULL UNIQUE
+);
+"""
+)
 # cursor.execute(
 #     "INSERT INTO users (username, email) VALUES ('alice', 'alice@email.com')"
 # )
